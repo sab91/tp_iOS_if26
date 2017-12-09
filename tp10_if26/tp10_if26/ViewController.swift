3a4217fc7ec2d08a2dfaa9f163e65a0116701446
sab91 <sabride@hotmail.fr>
2017-12-09 19:08:29 +0100

    TP10 complété

--- tp10_if26/tp10_if26/ViewController.swift
+++ tp10_if26/tp10_if26/ViewController.swift
@@ -12,8 +12,14 @@ import SQLite
 class ViewController: UIViewController {
     
     var database: Connection!
+    let usersTable = Table("users")
+    let id = Expression<Int>("id")
+    let name = Expression<String>("name")
+    let email = Expression<String>("email")
     
-    @IBAction func createTable(_ sender: UIButton) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         do {
             let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
             let fileUrl = documentDirectory.appendingPathComponent("users").appendingPathExtension("sqlite3")
@@ -24,11 +30,30 @@ class ViewController: UIViewController {
         } catch {
             print(error)
         }
-        print("create Table")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    @IBAction func createTable(_ sender: UIButton) {
+        let createTable = self.usersTable.create { (table) in
+            table.column(self.id, primaryKey: true)
+            table.column(self.name)
+            table.column(self.email, unique: true)
+        }
+        
+        do {
+            try self.database.run(createTable)
+            print("Created table")
+        } catch {
+            print(error)
+        }
+        //print("create Table")
     }
   
     @IBAction func insertUser(_ sender: UIButton) {
-        let alert  = UIAlertController(title: "Isert User", message: nil, preferredStyle: .alert)
+        let alert  = UIAlertController(title: "Insert User", message: nil, preferredStyle: .alert)
         alert.addTextField { (tf) in tf.placeholder = "Name"}
         alert.addTextField { (tf) in tf.placeholder = "Email"}
         let action = UIAlertAction(title: "Submit", style: .default) {
@@ -38,6 +63,15 @@ class ViewController: UIViewController {
                 else { return }
             print(name)
             print(email)
+            let insertUser = self.usersTable.insert(self.name <- name, self.email <- email)
+            
+            do {
+                try self.database.run(insertUser)
+                print("User inserted")
+            } catch {
+                print(error)
+            }
+            
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
@@ -45,17 +79,42 @@ class ViewController: UIViewController {
     }
     
     @IBAction func listUser(_ sender: UIButton) {
-        print("list user")
+        
+        do {
+            let users = try self.database.prepare(self.usersTable)
+            for user in users {
+                print("userId: \(user[self.id]), name: \(user[self.name]), email: \(user[self.email])")
+            }
+        } catch {
+            print(error)
+        }
+        //print("list user")
+        
     }
     
     @IBAction func updateUser(_ sender: UIButton) {
         let alert  = UIAlertController(title: "Update User", message: nil, preferredStyle: .alert)
-        alert.addTextField { (tf) in tf.placeholder = "ID"}
+        alert.addTextField { (tf) in tf.placeholder = "Name"}
+        alert.addTextField { (tf) in tf.placeholder = "Email"}
         let action = UIAlertAction(title: "Submit", style: .default) {
             (_)in guard
-                let id = alert.textFields?.first?.text
+                let name = alert.textFields?.first?.text,
+                let userId = Int(name),
+                let email = alert.textFields?.last?.text
                 else { return }
-            print(id)
+            print(name)
+            print(email)
+            
+            let user = self.usersTable.filter(self.id == userId)
+            let updateUser = user.update(self.email <- email)
+            
+            do {
+                try self.database.run(updateUser)
+                print("User updated")
+            } catch {
+                print(error)
+            }
+            
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
@@ -67,23 +126,26 @@ class ViewController: UIViewController {
         alert.addTextField { (tf) in tf.placeholder = "ID"}
         let action = UIAlertAction(title: "Submit", style: .default) {
             (_)in guard
-                let id = alert.textFields?.first?.text
+                let id = alert.textFields?.first?.text,
+                let userId = Int(id)
                 else { return }
             print(id)
+            
+            let user = self.usersTable.filter(self.id == userId)
+            let deleteUser = user.delete()
+            
+            do {
+                try self.database.run(deleteUser)
+            } catch {
+                print(error)
+            }
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         print("delete user")
     }
     
-    var fileUrl = docu
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
