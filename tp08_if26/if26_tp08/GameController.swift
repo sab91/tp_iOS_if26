bae96533710d5e635877a13c5f423c4736e0d410
sab91 <sabride@hotmail.fr>
2017-12-10 23:50:28 +0100

    TP08 complété

--- tp08_if26/if26_tp08/GameController.swift
+++ tp08_if26/if26_tp08/GameController.swift
@@ -1,63 +1,104 @@
 //
 //  GameController.swift
-//  if26_tp08
+//  Le Juste Nombre
 //
-//  Created by Sabri on 21/11/2017.
-//  Copyright © 2017 Sabri. All rights reserved.
+//  Téléchargé depuis http://semrom.fr/download/info/swift/22/GameControllerComplete.zip
+//  Copyright © 2017 Romain Semler. All rights reserved.
 //
 
 import UIKit
+
 class GameController: UIViewController {
     
     var nombreHasard: Int = 0
     var compteur: Int = 0
     var rejouer: Bool = false
     
-    @IBOutlet weak var valider: UIButton!
-    @IBOutlet weak var champsText: UITextField!
     @IBOutlet weak var resultat: UILabel!
+    @IBOutlet weak var champNombre: UITextField!
+    @IBOutlet weak var boutonValider: UIButton!
+    
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        valider.isEnabled = false
-        champsText.keyboardType = UIKeyboardType.numberPad
+        boutonValider.isEnabled = false
+        champNombre.keyboardType = UIKeyboardType.numberPad
         choisirNombreAuHasard()
     }
     
+    
+    
     func choisirNombreAuHasard() {
-        self.nombreHasard = Int(arc4random_uniform(100)) + 1
+        
+        self.nombreHasard = Int(arc4random_uniform(1000)) + 1
+    }
+    
+    
+    
+    @IBAction func enterNumber(_ sender: UITextField) {
+        if champNombre.text!.count != 0 {
+            boutonValider.isEnabled = true
+        } else {
+            boutonValider.isEnabled = false
+        }
+    }
+    
+    var borneinf: Int = 1
+    var bornesup: Int = 1000
+    
+    @IBAction func testerNombre(sender: UIButton) {
+        
+        if !self.rejouer {
+            
+            boutonValider.isEnabled = false
+            
+            let nombre: Int = Int(champNombre.text!)!
+            
+            if nombre < 1 || nombre > 1000 {
+                
+                resultat.text = "Merci d'entrer un nombre entre 1 et 1000."
+                
+            } else if nombre < nombreHasard {
+                
+                borneinf = nombre
+                resultat.text = "Le nombre est plus grand que \(nombre).\n Le nombre est compris entre \(borneinf) et \(bornesup)"
+                compteur += 1
+                
+            } else if nombre > nombreHasard {
+                
+                bornesup = nombre
+                resultat.text = "Le nombre est plus petit que \(nombre).\n Le nombre est compris entre \(borneinf) et \(bornesup)"
+                compteur += 1
+                
+            } else {
+                
+                compteur += 1
+                champNombre.isUserInteractionEnabled = false
+                resultat.text = "Bravo !\nVous avez trouvé le nombre \(nombreHasard).\n\nNombre d'essais : \(compteur)."
+                boutonValider.setTitle("Rejouer", for: UIControlState())
+                boutonValider.isEnabled = true
+                self.rejouer = true
+                
+            }
+            
+            champNombre.text = ""
+            
+        } else {
+            
+            self.rejouer = false
+            resultat.text = ""
+            champNombre.isUserInteractionEnabled = true
+            boutonValider.setTitle("Valider", for: UIControlState())
+            boutonValider.isEnabled = false
+            borneinf = 1
+            bornesup = 1000
+            self.compteur = 0
+            choisirNombreAuHasard()
+        }
         
     }
     
-    @IBAction func entrerNombre(_ sender: UITextField) {
-        if self.champsText.text!.characters.count != 0 {
-            valider.isEnabled = true
-        }
-        else {
-            valider.isEnabled = false
-        }
-    }
-    
-    
-    @IBAction func testerNombre(_ sender: UIButton) {
-        if rejouer == false {
-            valider.isEnabled = false
-            let nombre: Int = Int(champsText.text!)!
-            
-            if nombre < 1 && nombre > 100 {
-                print("Le nombre n'est pas compris entre 1 et 100")
-            }
-            else if nombre < nombreHasard {
-                print("Le nombre est plus grand que ", nombre)
-                compteur = compteur + 1
-            }
-            else if nombre > nombreHasard {
-                
-            }
-        }
-    }
-    
 }
-
 
